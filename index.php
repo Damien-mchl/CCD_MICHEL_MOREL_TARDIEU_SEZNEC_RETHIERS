6b06f337257dcce71aeac3695331cde60347403f
Loïc <tardieuloc@yahoo.fr>
2020-02-13 13:16:14 +0100

    Ajout des premières routes dans l'index

--- index.php
+++ index.php
@@ -10,3 +10,23 @@ $db = new EloquentManager();
 $db->addConnection(parse_ini_file("src/conf/conf.ini"));
 $db->setAsGlobal();
 $db->bootEloquent();
+
+$app->get("/liste_creneau", function() {
+    $creneauController= new CreneauController();
+    $creneauController->getListeCreneau();
+});
+
+$app->get("/liste_creneau/:idCreneau", function() use($app){
+    $creneauController= new CreneauController();
+    $creneauController->getCreneau();
+});
+
+$app->get("/liste_creneau/:idCreneau/ajoutBesoin", function() use($app){
+    $creneauController= new CreneauController();
+    $creneauController->getAttributionBesoinForm();
+});
+
+$app->post("/liste_creneau/:idCreneau/ajoutBesoin", function() use($app){
+    $creneauController= new CreneauController();
+    $creneauController->renderNewCrenau();
+});
