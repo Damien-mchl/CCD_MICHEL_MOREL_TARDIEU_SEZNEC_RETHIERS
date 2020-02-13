2b0aa98964962789d5cf90e74356668bf5a43d77
Loïc <tardieuloc@yahoo.fr>
2020-02-13 15:07:09 +0100

    Modification du controleur permanance

--- src/php/controleur/ControleurPermanence.php
+++ src/php/controleur/ControleurPermanence.php
@@ -2,10 +2,12 @@
 
 namespace GEG\controleur;
 
-use\GEG\vue\VuePermanance;
+use \GEG\vue\VuePermanance;
+use \GEG\modele\Crenau;
 
 class ControleurPermanance{
-    public function sinscrirCreneau($idCreneau, $role, $debut, $fin){
+    public function sinscrirCreneau($idCreneau, $role, $ponctuelle){
+        $cr= Crenau::were('id', '=', $idCreneau)->first();
 
     }
 }
